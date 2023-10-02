73f9f753308273f5cb85ecd2f47761bb01056b0f
CodingManoj <mvp2612@hotmail.com>
2023-10-01 22:38:59 -0400

    Git Auto Push

--- main.tf
+++ main.tf
@@ -33,9 +33,9 @@ module "mysql" {
 #     value   = module.redis.redis
 # }
 
-output "data" {
-  value = module.docdb.example
-}
+# output "data" {
+#   value = module.docdb.example
+# }
 
 # We cannot parametrize anything that's added n the source.
 # To limit that, we can use a tool called as Terrafile. All it does is clonnes the remote code from the specified branch and make it locally available.
